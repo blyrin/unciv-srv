d4f21041495f26d1951af3ac544020458211ec3d
FlapyPan <flapypan@gmail.com>
2025-02-21 12:34:45 +0800

    调整表结构

--- task.ts
+++ task.ts
@@ -2,15 +2,23 @@ import { sql } from './libs/db.ts'
 import { log } from './libs/log.ts'
 
 const cleanup = async () => {
-  const whitelist = (await sql`select "gameId" from "whitelist"`).map((f) => f.gameId)
-  log.info(`加载白名单，共 ${whitelist.length} 个`)
-  const result = await sql`delete
-                           from "files"
-                           where "gameId" not in ${sql(whitelist)}
-                             and "updatedAt" < (now() - interval '3 months')
-                           returning "gameId"`
-  const deletedGameIds = result.map((r) => r.gameId)
-  log.info(`清理完成, 共删除 ${deletedGameIds.length} 个`, deletedGameIds.join(','))
+  const [deletedGameCount, deletedPlayerCount] = await sql.begin(async (sql) => {
+    const deletedGames = await sql`
+        delete
+        from "files"
+        where "whitelist" = false
+          and "updatedAt" < (now() - interval '3 months')
+        returning "gameId"`
+    const deletedPlayers = await sql`
+        delete
+        from "players"
+        where "whitelist" = false
+          and "playerId" not in (select distinct (jsonb_array_elements_text("playerIds"))::uuid from "files")
+          and "updatedAt" < (now() - interval '3 months')
+        returning "playerId"`
+    return [deletedGames.length, deletedPlayers.length]
+  })
+  log.info(`清理完成, 共删除 ${deletedGameCount} 个存档, ${deletedPlayerCount} 个玩家`)
 }
 
 export const startTask = async () => {
